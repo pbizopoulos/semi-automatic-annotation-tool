b6f894ef28e44d719415358ac729e38c8711b399
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-05-04 19:04:56 +0300

    update test

--- test/test.js
+++ test/test.js
@@ -5,28 +5,23 @@ const path = require('path');
 const puppeteer = require('puppeteer');
 
 (async () => {
-	const browser = await puppeteer.launch({ headless: true });
+	const browser = await puppeteer.launch({ headless: true, slowMo: 300 });
 	const page = await browser.newPage();
 	await page.goto(`file:${path.join(__dirname, '../index.html')}`);
-	await page.waitForTimeout(1000);
 	await page.waitForSelector('#modelSelect:not([disabled])');
 	await page.evaluate(() => {
 		document.querySelector('#modelSelect').selectedIndex = 1;
 		document.querySelector('#modelSelect').onchange();
 	});
-	await page.waitForTimeout(1000);
 	await page.waitForSelector('#loadImagesInputFile:not([disabled])');
 	const inputUploadHandle = await page.$('#loadImagesInputFile');
 	const niftiFileName = 'artifacts/val_im.nii.gz';
 	const nifti = fs.readFileSync(niftiFileName);
 	inputUploadHandle.uploadFile(niftiFileName);
-	await page.waitForTimeout(1000);
 	await page.waitForSelector('#modelSelect:not([disabled])');
-	await page.evaluate(() => {
-		document.querySelector('#labelColorDiv1').click();
-		document.querySelector('#predictCurrentImageButton').click();
-	});
-	await page.waitForTimeout(10000);
+	await page.waitForSelector('#labelColorDiv1').then(selector => selector.click());
+	await page.waitForSelector('#predictCurrentImageButton').then(selector => selector.click());
+	await page.waitForTimeout(5000);
 	await page.screenshot({
 		path: 'artifacts/test.png',
 		fullpage: true
