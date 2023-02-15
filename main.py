3d9864ef5db3bbecc00d14ee429788a3fb8d3fc0
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-02-15 22:05:31 +0200

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from hashlib import sha256
-from os.path import isfile, join
+from pathlib import Path
 from zipfile import ZipFile
 
 import gdown
@@ -7,12 +7,11 @@ from playwright.sync_api import sync_playwright
 
 
 def main() -> None:
-    zip_file_path = join('bin', 'rp_im.zip')
-    if not isfile(zip_file_path):
-        gdown.download('https://drive.google.com/uc?id=1ruTiKdmqhqdbE9xOEmjQGing76nrTK2m', zip_file_path, quiet=False)
+    zip_file_path = Path('bin/rp_im.zip')
+    if not zip_file_path.is_file():
+        gdown.download('https://drive.google.com/uc?id=1ruTiKdmqhqdbE9xOEmjQGing76nrTK2m', zip_file_path.as_posix(), quiet=False)
         with ZipFile(zip_file_path, 'r') as zip_file:
             zip_file.extractall('bin')
-    zip_file_path = join('bin', 'latest.zip')
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(args=['--use-gl=egl'])
         context = browser.new_context(record_video_dir='bin/')
@@ -23,20 +22,20 @@ def main() -> None:
         page.on('pageerror', lambda exception: (_ for _ in ()).throw(Exception(f'uncaught exception: {exception}')))
         page.goto('file:///usr/src/app/docs/index.html')
         page.locator('#model-download-div').wait_for(state='hidden')
-        page.set_input_files('#load-files-input-file', join('bin', 'rp_im', '1.nii.gz'))
+        page.set_input_files('#load-files-input-file', 'bin/rp_im/1.nii.gz')
         page.locator('#image-index-input-range').fill('2')
         page.locator('#label-color-div-1').click()
-        page.screenshot(path=join('bin', 'before.png'))
+        page.screenshot(path='bin/before.png')
         page.locator('#reset-image-value-button').click()
         page.locator('#predict-image-current-button').click()
         with page.expect_download() as download_info:
             page.click('#save-predictions-to-disk-button')
         download = download_info.value
-        download.save_as(join('bin', 'masks.nii'))
-        with open(join('bin', 'masks.nii'), 'rb') as file:
+        download.save_as('bin/masks.nii')
+        with Path('bin/masks.nii').open('rb') as file:
             assert sha256(file.read()).hexdigest() == '6d1f1c28c38cab797d7500b01e5379223229b63c44bc857cbb38aab75fef75f2'
-        page.screenshot(path=join('bin', 'screenshot.png'))
-        with open(join('bin', 'screenshot.png'), 'rb') as file:
+        page.screenshot(path='bin/screenshot.png')
+        with Path('bin/screenshot.png').open('rb') as file:
             assert sha256(file.read()).hexdigest() == '664ba9b46e091ec97db7d60dbac519329e5aacbac9a3f9cc0d194efb02bdc105'
         context.close()
         browser.close()
