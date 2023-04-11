0b36d7a21d1a9d3295dd528f3599e77a18e1eb3a
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-04-11 19:25:14 +0300

    .

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM mcr.microsoft.com/playwright/python:v1.32.1-focal
-ENV HOME=/usr/src/app/bin
-ENV PYTHONDONTWRITEBYTECODE=1
-WORKDIR /usr/src/app
-COPY pyproject.toml .
-RUN python3 -m pip install --upgrade pip && python3 -m pip install .[dev]
--- python/main.py
+++ python/main.py
@@ -1,50 +1,48 @@
-import io
-from os import environ
+from hashlib import sha256
+from pathlib import Path
+from zipfile import ZipFile
 
-import tensorflowjs as tfjs
-from flask import Flask, Response, request
-from flask_cors import CORS, cross_origin
-from werkzeug import formparser
-
-
-class ModelReceiver():
-
-    def stream_factory(self: "ModelReceiver", total_content_length, content_type, filename, content_length=None): # type: ignore[no-untyped-def] # noqa: ANN001, ANN201, ARG002
-        if filename == "model.json":
-            self.model_json_bytes = io.BytesIO()
-            self.model_json_writer = io.BufferedWriter(self.model_json_bytes) # type: ignore[arg-type]
-            return self.model_json_writer
-        if filename == "model.weights.bin":
-            self.weight_bytes = io.BytesIO()
-            self.weight_writer = io.BufferedWriter(self.weight_bytes) # type: ignore[arg-type]
-            return self.weight_writer
-        return None
+import gdown
+from playwright.sync_api import Error, sync_playwright
 
 
 def main() -> None:
-    app = Flask("model-server")
-    CORS(app)
-    app.config["CORS_HEADER"] = "Content-Type"
-    model_receiver = ModelReceiver()
+    zip_file_path = Path("bin/rp_im.zip")
+    if not zip_file_path.is_file():
+        gdown.download("https://drive.google.com/uc?id=1ruTiKdmqhqdbE9xOEmjQGing76nrTK2m", zip_file_path.as_posix(), quiet=False)
+        with ZipFile(zip_file_path, "r") as zip_file:
+            zip_file.extractall("bin")
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(args=["--use-gl=egl"])
+        context = browser.new_context(record_video_dir="bin/")
+        page = context.new_page()
+        timeout = 100000
+        page.set_default_timeout(timeout)
+        page.set_default_navigation_timeout(timeout)
+        page.on("pageerror", page_error)
+        page.goto("https://semi-automatic-annotation-tool.incisive.iti.gr/")
+        page.locator("#model-download-div").wait_for(state="hidden")
+        page.set_input_files("#load-files-input-file", "bin/rp_im/1.nii.gz")
+        page.locator("#image-index-input-range").fill("2")
+        page.locator("#label-color-div-1").click()
+        page.screenshot(path="bin/before.png")
+        page.locator("#reset-image-value-button").click()
+        page.locator("#predict-image-current-button").click()
+        with page.expect_download() as download_info:
+            page.click("#save-predictions-to-disk-button")
+        download = download_info.value
+        download.save_as("bin/masks.nii")
+        with Path("bin/masks.nii").open("rb") as file:
+            assert sha256(file.read()).hexdigest() == "6d1f1c28c38cab797d7500b01e5379223229b63c44bc857cbb38aab75fef75f2"
+        page.screenshot(path="bin/screenshot.png")
+        with Path("bin/screenshot.png").open("rb") as file:
+            assert sha256(file.read()).hexdigest() == "3b1ca1c1902d8ab87f1f23cecf58e565e5afba357cd763ead3b143a159675ab5"
+        context.close()
+        browser.close()
 
-    @app.route("/upload", methods=["POST"])
-    @cross_origin() # type: ignore[misc]
-    def upload() -> Response:
-        formparser.parse_form_data(request.environ, stream_factory=model_receiver.stream_factory)
-        model_receiver.model_json_writer.flush()
-        model_receiver.weight_writer.flush()
-        model_receiver.model_json_writer.seek(0)
-        model_receiver.weight_writer.seek(0)
-        json_content = model_receiver.model_json_bytes.read()
-        weights_content = model_receiver.weight_bytes.read()
-        model = tfjs.converters.deserialize_keras_model(json_content, weight_data=[weights_content])
-        model.summary()
-        # You can perform `model.predict()`, `model.fit()`,
-        # `model.evaluate()` etc. here.
-        return Response(status=200)
 
-    if environ["DEBUG"] != "1":
-        app.run("0.0.0.0", 5000) # noqa: S104
+def page_error(exception: Error) -> None:
+    raise exception
 
 
 if __name__ == "__main__":
